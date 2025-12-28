d43e44de96710577151f67a4759edc51ef2df934
Deeporacle2410 <165961688+Deeporacle2410@users.noreply.github.com>
2025-12-28 05:35:55 +0100

    created shared function

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call(string url,string branch){
+  echo "cloning is in progress"
+  git url: "https://github.com/Deeporacle2410/django-notes-app.git", branch: "main"
+  echo "cloning complted succesfully"
+}
